9b9c5246d6207983fb60bcb488d58688ec369b70
yamavol <yamavol@outlook.com>
2020-11-06 02:21:21 +0900

    formatted code

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@
  */
 
 
- /** } */
+/** } */
 const createTEX2HTML = require('./tex2html');
 
 createTEX2HTML()
@@ -22,7 +22,7 @@ createTEX2HTML()
 top = true;
 
 const DEFAULT_OPTIONS = {
-    throwOnError: true
+  throwOnError: true
 };
 
 /**
@@ -32,28 +32,28 @@ const DEFAULT_OPTIONS = {
  * @param {number} pos 
  */
 function isValidDelim(state, pos) {
-    var prevChar, nextChar,
-        max = state.posMax,
-        can_open = true,
-        can_close = true;
+  var prevChar, nextChar,
+    max = state.posMax,
+    can_open = true,
+    can_close = true;
 
-    prevChar = pos > 0 ? state.src.charCodeAt(pos - 1) : -1;
-    nextChar = pos + 1 <= max ? state.src.charCodeAt(pos + 1) : -1;
+  prevChar = pos > 0 ? state.src.charCodeAt(pos - 1) : -1;
+  nextChar = pos + 1 <= max ? state.src.charCodeAt(pos + 1) : -1;
 
-    // Check non-whitespace conditions for opening and closing, and
-    // check that closing delimeter isn't followed by a number
-    if (prevChar === 0x20/* " " */ || prevChar === 0x09/* \t */ ||
-            (nextChar >= 0x30/* "0" */ && nextChar <= 0x39/* "9" */)) {
-        can_close = false;
-    }
-    if (nextChar === 0x20/* " " */ || nextChar === 0x09/* \t */) {
-        can_open = false;
-    }
+  // Check non-whitespace conditions for opening and closing, and
+  // check that closing delimeter isn't followed by a number
+  if (prevChar === 0x20/* " " */ || prevChar === 0x09/* \t */ ||
+    (nextChar >= 0x30/* "0" */ && nextChar <= 0x39/* "9" */)) {
+    can_close = false;
+  }
+  if (nextChar === 0x20/* " " */ || nextChar === 0x09/* \t */) {
+    can_open = false;
+  }
 
-    return {
-        can_open: can_open,
-        can_close: can_close
-    };
+  return {
+    can_open: can_open,
+    can_close: can_close
+  };
 }
 
 /**
@@ -61,64 +61,64 @@ function isValidDelim(state, pos) {
  * @param {boolean} silent 
  */
 function math_inline(state, silent) {
-    var start, match, token, res, pos, esc_count;
+  var start, match, token, res, pos, esc_count;
 
-    if (state.src[state.pos] !== "$") { return false; }
+  if (state.src[state.pos] !== "$") { return false; }
 
-    res = isValidDelim(state, state.pos);
-    if (!res.can_open) {
-        if (!silent) { state.pending += "$"; }
-        state.pos += 1;
-        return true;
-    }
-
-    // First check for and bypass all properly escaped delimieters
-    // This loop will assume that the first leading backtick can not
-    // be the first character in state.src, which is known since
-    // we have found an opening delimieter already.
-    start = state.pos + 1;
-    match = start;
-    while ( (match = state.src.indexOf("$", match)) !== -1) {
-        // Found potential $, look for escapes, pos will point to
-        // first non escape when complete
-        pos = match - 1;
-        while (state.src[pos] === "\\") { pos -= 1; }
-
-        // Even number of escapes, potential closing delimiter found
-        if ( ((match - pos) % 2) == 1 ) { break; }
-        match += 1;
-    }
-
-    // No closing delimter found.  Consume $ and continue.
-    if (match === -1) {
-        if (!silent) { state.pending += "$"; }
-        state.pos = start;
-        return true;
-    }
-
-    // Check if we have empty content, ie: $$.  Do not parse.
-    if (match - start === 0) {
-        if (!silent) { state.pending += "$$"; }
-        state.pos = start + 1;
-        return true;
-    }
-
-    // Check for valid closing delimiter
-    res = isValidDelim(state, match);
-    if (!res.can_close) {
-        if (!silent) { state.pending += "$"; }
-        state.pos = start;
-        return true;
-    }
-
-    if (!silent) {
-        token         = state.push('math_inline', 'math', 0);
-        token.markup  = "$";
-        token.content = state.src.slice(start, match);
-    }
-
-    state.pos = match + 1;
+  res = isValidDelim(state, state.pos);
+  if (!res.can_open) {
+    if (!silent) { state.pending += "$"; }
+    state.pos += 1;
     return true;
+  }
+
+  // First check for and bypass all properly escaped delimieters
+  // This loop will assume that the first leading backtick can not
+  // be the first character in state.src, which is known since
+  // we have found an opening delimieter already.
+  start = state.pos + 1;
+  match = start;
+  while ((match = state.src.indexOf("$", match)) !== -1) {
+    // Found potential $, look for escapes, pos will point to
+    // first non escape when complete
+    pos = match - 1;
+    while (state.src[pos] === "\\") { pos -= 1; }
+
+    // Even number of escapes, potential closing delimiter found
+    if (((match - pos) % 2) == 1) { break; }
+    match += 1;
+  }
+
+  // No closing delimter found.  Consume $ and continue.
+  if (match === -1) {
+    if (!silent) { state.pending += "$"; }
+    state.pos = start;
+    return true;
+  }
+
+  // Check if we have empty content, ie: $$.  Do not parse.
+  if (match - start === 0) {
+    if (!silent) { state.pending += "$$"; }
+    state.pos = start + 1;
+    return true;
+  }
+
+  // Check for valid closing delimiter
+  res = isValidDelim(state, match);
+  if (!res.can_close) {
+    if (!silent) { state.pending += "$"; }
+    state.pos = start;
+    return true;
+  }
+
+  if (!silent) {
+    token = state.push('math_inline', 'math', 0);
+    token.markup = "$";
+    token.content = state.src.slice(start, match);
+  }
+
+  state.pos = match + 1;
+  return true;
 }
 
 /**
@@ -128,56 +128,56 @@ function math_inline(state, silent) {
  * @param {number} end 
  * @param {boolean} silent 
  */
-function math_block(state, start, end, silent){
-    var firstLine, lastLine, next, lastPos, found = false, token,
-        pos = state.bMarks[start] + state.tShift[start],
-        max = state.eMarks[start]
+function math_block(state, start, end, silent) {
+  var firstLine, lastLine, next, lastPos, found = false, token,
+    pos = state.bMarks[start] + state.tShift[start],
+    max = state.eMarks[start]
 
-    if(pos + 2 > max){ return false; }
-    if(state.src.slice(pos,pos+2)!=='$$'){ return false; }
+  if (pos + 2 > max) { return false; }
+  if (state.src.slice(pos, pos + 2) !== '$$') { return false; }
 
-    pos += 2;
-    firstLine = state.src.slice(pos,max);
+  pos += 2;
+  firstLine = state.src.slice(pos, max);
 
-    if(silent){ return true; }
-    if(firstLine.trim().slice(-2)==='$$'){
-        // Single line expression
-        firstLine = firstLine.trim().slice(0, -2);
-        found = true;
+  if (silent) { return true; }
+  if (firstLine.trim().slice(-2) === '$$') {
+    // Single line expression
+    firstLine = firstLine.trim().slice(0, -2);
+    found = true;
+  }
+
+  for (next = start; !found;) {
+
+    next++;
+
+    if (next >= end) { break; }
+
+    pos = state.bMarks[next] + state.tShift[next];
+    max = state.eMarks[next];
+
+    if (pos < max && state.tShift[next] < state.blkIndent) {
+      // non-empty line with negative indent should stop the list:
+      break;
     }
 
-    for(next = start; !found; ){
-
-        next++;
-
-        if(next >= end){ break; }
-
-        pos = state.bMarks[next]+state.tShift[next];
-        max = state.eMarks[next];
-
-        if(pos < max && state.tShift[next] < state.blkIndent){
-            // non-empty line with negative indent should stop the list:
-            break;
-        }
-
-        if(state.src.slice(pos,max).trim().slice(-2)==='$$'){
-            lastPos = state.src.slice(0,max).lastIndexOf('$$');
-            lastLine = state.src.slice(pos,lastPos);
-            found = true;
-        }
-
+    if (state.src.slice(pos, max).trim().slice(-2) === '$$') {
+      lastPos = state.src.slice(0, max).lastIndexOf('$$');
+      lastLine = state.src.slice(pos, lastPos);
+      found = true;
     }
 
-    state.line = next + 1;
+  }
 
-    token = state.push('math_block', 'math', 0);
-    token.block = true;
-    token.content = (firstLine && firstLine.trim() ? firstLine + '\n' : '')
+  state.line = next + 1;
+
+  token = state.push('math_block', 'math', 0);
+  token.block = true;
+  token.content = (firstLine && firstLine.trim() ? firstLine + '\n' : '')
     + state.getLines(start + 1, next, state.tShift[start], true)
     + (lastLine && lastLine.trim() ? lastLine : '');
-    token.map = [ start, state.line ];
-    token.markup = '$$';
-    return true;
+  token.map = [start, state.line];
+  token.markup = '$$';
+  return true;
 }
 
 
@@ -187,11 +187,11 @@ function math_block(state, start, end, silent){
  * @param {Partial<PluginOptions>} options 
  */
 function InitPlugin(instance, options) {
-    options = Object.assign({}, options, DEFAULT_OPTIONS);
-    options.mathjax = options.mathjax || {};
+  options = Object.assign({}, options, DEFAULT_OPTIONS);
+  options.mathjax = options.mathjax || {};
 
-    instance.options = options;
-    instance.tex2html = createTEX2HTML(options.mathjax);
+  instance.options = options;
+  instance.tex2html = createTEX2HTML(options.mathjax);
 }
 
 /**
@@ -199,24 +199,24 @@ function InitPlugin(instance, options) {
  * @param {Partial<PluginOptions>} options 
  */
 function MathjaxPlugin(options) {
-    if (!(this instanceof MathjaxPlugin)) {
-        return new MathjaxPlugin(options);
-    }
-    options = Object.assign({}, options, DEFAULT_OPTIONS);
-    options.mathjax = options.mathjax || {};
+  if (!(this instanceof MathjaxPlugin)) {
+    return new MathjaxPlugin(options);
+  }
+  options = Object.assign({}, options, DEFAULT_OPTIONS);
+  options.mathjax = options.mathjax || {};
 
-    /** @name MathjaxPlugin#options */
-    this.options = options;
+  /** @name MathjaxPlugin#options */
+  this.options = options;
 
-    /** @name MathjaxPlugin#tex2html */
-    this.tex2html = createTEX2HTML(options.mathjax);
+  /** @name MathjaxPlugin#tex2html */
+  this.tex2html = createTEX2HTML(options.mathjax);
 }
 
 /**
  * @param {Partial<PluginOptions>} options
  */
-MathjaxPlugin.prototype.init = function(options) {
-    InitPlugin(this, options);
+MathjaxPlugin.prototype.init = function (options) {
+  InitPlugin(this, options);
 }
 
 
@@ -224,71 +224,71 @@ MathjaxPlugin.prototype.init = function(options) {
  * Returns a markdown-it plugin
  * @return { Plugin }
  */
-MathjaxPlugin.prototype.plugin = function() {
-    const self = this;
-    return (md, options) => {
-        if (options !== undefined) {
-          InitPlugin(self, options);
-        }
-        /**
-         * Inline RenderRule Implementation
-         * @param {string} tex  equation
-         */
-        var mathjaxInline = function(tex){
-            self.options.displayMode = false;
-            self.options.mathjax.inline = !self.options.displayMode;
-            try{
-                var math = self.tex2html.convert(tex, self.options.mathjax);
-                return math; 
-            }
-            catch(error){
-                if(self.options.throwOnError){ console.log(error); }
-                return tex;
-            }
-        };
-
-        /**
-         * @type {RenderRule}
-         */
-        var inlineRenderer = function(tokens, idx){
-            return mathjaxInline(tokens[idx].content);
-        };
-
-        /**
-         * Block RenderRule Implementation
-         * @param {string} tex 
-         */
-        var mathjaxBlock = function(tex){
-            self.options.displayMode = true;
-            self.options.mathjax.inline = !self.options.displayMode;
-            try{
-                var math = self.tex2html.convert(tex, self.options.mathjax);
-                return "<p>" + math + "</p>";
-            }
-            catch(error){
-                if(self.options.throwOnError){ console.log(error); }
-                return tex;
-            }
-        }
-
-        /**
-         * @type {RenderRule}
-         */
-        var blockRenderer = function(tokens, idx){
-            return  mathjaxBlock(tokens[idx].content) + '\n';
-        }
-
-        md.inline.ruler.after('escape', 'math_inline', math_inline);
-        md.block.ruler.after('blockquote', 'math_block', math_block, {
-            alt: [ 'paragraph', 'reference', 'blockquote', 'list' ]
-        });
-        md.renderer.rules.math_inline = inlineRenderer;
-        md.renderer.rules.math_block = blockRenderer;
+MathjaxPlugin.prototype.plugin = function () {
+  const self = this;
+  return (md, options) => {
+    if (options !== undefined) {
+      InitPlugin(self, options);
     }
+    /**
+     * Inline RenderRule Implementation
+     * @param {string} tex  equation
+     */
+    var mathjaxInline = function (tex) {
+      self.options.displayMode = false;
+      self.options.mathjax.inline = !self.options.displayMode;
+      try {
+        var math = self.tex2html.convert(tex, self.options.mathjax);
+        return math;
+      }
+      catch (error) {
+        if (self.options.throwOnError) { console.log(error); }
+        return tex;
+      }
+    };
+
+    /**
+     * @type {RenderRule}
+     */
+    var inlineRenderer = function (tokens, idx) {
+      return mathjaxInline(tokens[idx].content);
+    };
+
+    /**
+     * Block RenderRule Implementation
+     * @param {string} tex 
+     */
+    var mathjaxBlock = function (tex) {
+      self.options.displayMode = true;
+      self.options.mathjax.inline = !self.options.displayMode;
+      try {
+        var math = self.tex2html.convert(tex, self.options.mathjax);
+        return "<p>" + math + "</p>";
+      }
+      catch (error) {
+        if (self.options.throwOnError) { console.log(error); }
+        return tex;
+      }
+    }
+
+    /**
+     * @type {RenderRule}
+     */
+    var blockRenderer = function (tokens, idx) {
+      return mathjaxBlock(tokens[idx].content) + '\n';
+    }
+
+    md.inline.ruler.after('escape', 'math_inline', math_inline);
+    md.block.ruler.after('blockquote', 'math_block', math_block, {
+      alt: ['paragraph', 'reference', 'blockquote', 'list']
+    });
+    md.renderer.rules.math_inline = inlineRenderer;
+    md.renderer.rules.math_block = blockRenderer;
+  }
 };
 
-MathjaxPlugin.prototype.getCSS = function() {
-    return this.tex2html.getCSS();
+MathjaxPlugin.prototype.getCSS = function () {
+  return this.tex2html.getCSS();
 }
 
 module.exports = MathjaxPlugin;
--- src/tex2html.js
+++ src/tex2html.js
@@ -4,12 +4,12 @@
  * https://github.com/mathjax/mj3-demos-node/blob/master/mj3-tex2html
  * 
  */
-const {mathjax} = require('mathjax-full/js/mathjax');
-const {TeX} = require('mathjax-full/js/input/tex');
-const {CHTML} = require('mathjax-full/js/output/chtml');
-const {liteAdaptor} = require('mathjax-full/js/adaptors/liteAdaptor');
-const {AllPackages} = require('mathjax-full/js/input/tex/AllPackages');
-const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
+const { mathjax } = require('mathjax-full/js/mathjax');
+const { TeX } = require('mathjax-full/js/input/tex');
+const { CHTML } = require('mathjax-full/js/output/chtml');
+const { liteAdaptor } = require('mathjax-full/js/adaptors/liteAdaptor');
+const { AllPackages } = require('mathjax-full/js/input/tex/AllPackages');
+const { RegisterHTMLHandler } = require('mathjax-full/js/handlers/html.js');
 
 /**
  * @template T
@@ -18,7 +18,7 @@ const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
  * @return {T}
  */
 function useParam(param, defaultParam) {
-    return param !== undefined ? param : defaultParam;
+  return param !== undefined ? param : defaultParam;
 }
 
 /**
@@ -39,59 +39,59 @@ const MATHJAX_DEFAULT_FONT_URL = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/out
  * 
  * @param {Partial<MathJaxConfig>=} opts 
  */
-module.exports = function(opts) {
-    // set default conversion parameters
-    opts        = opts            || {};
-    opts.inline = opts.inline     || false;
-    opts.em     = opts.em         || 16;
-    opts.ex     = opts.ex         || 8;
-    opts.width  = opts.width      || 80*16;
-    opts.scale  = opts.scale      || 1.21; // magic # chosen which looks better.
-    opts.fontURL= opts.fontURL    || MATHJAX_DEFAULT_FONT_URL;
-    opts.packages = opts.packages || AllPackages.sort().join(', ');
-    opts.adaptiveCSS = opts.adaptiveCSS || true;
+module.exports = function (opts) {
+  // set default conversion parameters
+  opts = opts || {};
+  opts.inline = opts.inline || false;
+  opts.em = opts.em || 16;
+  opts.ex = opts.ex || 8;
+  opts.width = opts.width || 80 * 16;
+  opts.scale = opts.scale || 1.21; // magic # chosen which looks better.
+  opts.fontURL = opts.fontURL || MATHJAX_DEFAULT_FONT_URL;
+  opts.packages = opts.packages || AllPackages.sort().join(', ');
+  opts.adaptiveCSS = opts.adaptiveCSS || true;
 
-    //
-    // set up mathjax and conversion function
-    //
-    const adaptor = liteAdaptor();
-    RegisterHTMLHandler(adaptor);
-    const tex = new TeX({ 
-        packages: opts.packages.split(/\s*,\s*/)
-    });
-    const chtml = new CHTML({
-        fontURL: opts.fontURL,
-        exFactor: opts.ex / opts.em,
-        adaptiveCSS: opts.adaptiveCSS,
-    });
-    const html = mathjax.document('', {
-        InputJax: tex,
-        OutputJax: chtml
-    });
+  //
+  // set up mathjax and conversion function
+  //
+  const adaptor = liteAdaptor();
+  RegisterHTMLHandler(adaptor);
+  const tex = new TeX({
+    packages: opts.packages.split(/\s*,\s*/)
+  });
+  const chtml = new CHTML({
+    fontURL: opts.fontURL,
+    exFactor: opts.ex / opts.em,
+    adaptiveCSS: opts.adaptiveCSS,
+  });
+  const html = mathjax.document('', {
+    InputJax: tex,
+    OutputJax: chtml
+  });
 
-    /**
-     * Function which converts input 
-     * @param {string}          tex      TeX equation
-     * @param {MathJaxConfig}   param    conversion parameters
-     */
-    const Convert = (tex, param) => {
-        param = param || opts;
-        const node = html.convert(tex || '', {
-            display:       !useParam(param.inline, opts.inline),
-            em:             useParam(param.em    , opts.em),
-            ex:             useParam(param.ex    , opts.ex),
-            containerWidth: useParam(param.width , opts.width),
-            scale:          useParam(param.scale , opts.scale),
-        })
-        return adaptor.outerHTML(node);
-    }
+  /**
+   * Function which converts input 
+   * @param {string}          tex      TeX equation
+   * @param {MathJaxConfig}   param    conversion parameters
+   */
+  const Convert = (tex, param) => {
+    param = param || opts;
+    const node = html.convert(tex || '', {
+      display: !useParam(param.inline, opts.inline),
+      em: useParam(param.em, opts.em),
+      ex: useParam(param.ex, opts.ex),
+      containerWidth: useParam(param.width, opts.width),
+      scale: useParam(param.scale, opts.scale),
+    })
+    return adaptor.outerHTML(node);
+  }
 
-    const GetCSS = () => {
-        return adaptor.textContent(chtml.styleSheet(html));
-    }
+  const GetCSS = () => {
+    return adaptor.textContent(chtml.styleSheet(html));
+  }
 
-    return {
-        convert: Convert,
-        getCSS: GetCSS,
-    }
+  return {
+    convert: Convert,
+    getCSS: GetCSS,
+  }
 };
